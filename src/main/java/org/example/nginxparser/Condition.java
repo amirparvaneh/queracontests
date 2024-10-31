aff82fc5a1548f1b5c170c643246a72f08d44a06
amirparvaneh <amirparvaneh138@gmail.com>
2024-10-31 10:55:57 +0330

    [feature] nginxparser start project

---
+++ src/main/java/org/example/nginxparser/Condition.java
@@ -0,0 +1,61 @@
+public class Condition {
+    public static Condition equal(String propertyName, String value) {
+        // TODO: Implement
+        return null;
+    }
+
+    public static Condition notEqual(String propertyName, String value) {
+        // TODO: Implement
+        return null;
+    }
+
+    public static Condition in(String propertyName, String... values) {
+        // TODO: Implement
+        return null;
+    }
+
+    public static Condition notIn(String propertyName, String... values) {
+        // TODO: Implement
+        return null;
+    }
+
+    public static Condition lessThan(String propertyName, Long value) {
+        // TODO: Implement
+        return null;
+    }
+
+    public static Condition lessOrEqual(String propertyName, Long value) {
+        // TODO: Implement
+        return null;
+    }
+
+    public static Condition greaterThan(String propertyName, Long value) {
+        // TODO: Implement
+        return null;
+    }
+
+    public static Condition greaterOrEqual(String propertyName, Long value) {
+        // TODO: Implement
+        return null;
+    }
+
+    public Condition and(Condition condition) {
+        // TODO: Implement
+        return null;
+    }
+
+    public Condition or(Condition condition) {
+        // TODO: Implement
+        return null;
+    }
+
+    public static Condition not(Condition condition) {
+        // TODO: Implement
+        return null;
+    }
+
+    public boolean test(Log log) {
+        // TODO: Implement
+        return false;
+    }
+}
